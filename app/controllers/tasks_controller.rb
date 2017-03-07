24d17fa3f4c47b0c7d36ce78ef6a1c9fa10a18ad
Guillaume Salin <gsalin@gmail.com>
2017-03-07 12:48:21 +0100

    didn't finish the beginning yet

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # @task = ? --------------------
+  end
+
+  def edit
+     @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    # @task.?
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+end
